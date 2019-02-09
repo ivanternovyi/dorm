ce68b889dadc4b7946599b77f492ec2abe221056
Ivan Ternovyi <ivanternovyi@gmail.com>
2019-02-09 17:33:15 +0200

    js architecture for coffee script components

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require init
+//= require lib/ui_component
+//= require components/test_component
+//= require jquery
