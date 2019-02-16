97e70eb77c23f9fb1aac9c21f41e235e24134f8c
Ivan Ternovyi <ivanternovyi@gmail.com>
2019-02-16 19:27:35 +0200

    add admin role

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190209161717) do
+ActiveRecord::Schema.define(version: 20190216171509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",               default: "", null: false
+    t.string   "encrypted_password",  default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
+  end
 
   create_table "students", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
