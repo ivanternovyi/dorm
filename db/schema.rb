8e6393f3851b7289d53bc25d4876b068aa961ae5
Ivan Ternovyi <ivanternovyi@gmail.com>
2019-02-17 14:38:26 +0200

    add avatar uploader for student

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190217121943) do
+ActiveRecord::Schema.define(version: 20190217123536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20190217121943) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "student_id"
+    t.string   "avatar"
     t.index ["email"], name: "index_students_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true, using: :btree
   end
